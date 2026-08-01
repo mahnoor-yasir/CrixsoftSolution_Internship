bc2a7f43bb654c944a1aae2d55fe6f978d388226
Mahnoor Yasir <167002467+mahnoor-yasir@users.noreply.github.com>
2026-08-01 20:38:51 +0500

    Create Main.java

---
+++ MovieDatabaseSystem/src/com/moviedb/Main.java
@@ -0,0 +1,10 @@
+package com.moviedb;
+
+import com.moviedb.ui.ConsoleUI;
+
+public class Main {
+    public static void main(String[] args) {
+        ConsoleUI ui = new ConsoleUI();
+        ui.start();
+    }
+}
